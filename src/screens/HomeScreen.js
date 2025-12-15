10ecc3fb861126d9717375bae9fb18d5d07f65b7
Rommel Dijon <bartmilhausky@gmail.com>
2025-12-16 08:27:41 +1300

    Rendered Categories component & passed 3 props: categories, activeCategory, and handeleChangeCategory

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -328,7 +328,11 @@ export default function HomeScreen() {
         </View>
 
         <View testID="categoryList">
-        
+          <Categories
+            categories={categories}
+            activeCategory={activeCategory}
+            handleChangeCategory={handleChangeCategory}
+          />
         </View>
 
         <View testID="articleList">
