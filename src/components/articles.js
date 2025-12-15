506d3327ca0243cb5144dc2f32163ecb16de3544
Rommel Dijon <bartmilhausky@gmail.com>
2025-12-16 10:48:46 +1300

    Refactored and corrected codes

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,20 +1,40 @@
-import { View, Text, Pressable, Image, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp,} from "react-native-responsive-screen";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 import { useNavigation } from "@react-navigation/native";
 
-export default function Articles({ categories, articles }) {
+export default function Articles({ articles }) {
   const navigation = useNavigation();
 
   const renderItem = ({ item, index }) => (
-<ArticleCard item={item} index={index} navigation={navigation} />
+    <ArticleCard item={item} index={index} navigation={navigation} />
   );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title} testID="title">Latest News</Text>
+      <Text style={styles.title} testID="title">
+        Latest News
+      </Text>
+
       <View testID="articlesDisplay">
-     
+        <FlatList
+          data={articles}
+          keyExtractor={(item) => item.idArticle}
+          renderItem={renderItem}
+          numColumns={2}
+          columnWrapperStyle={styles.row}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
     </View>
   );
@@ -23,43 +43,43 @@ export default function Articles({ categories, articles }) {
 const ArticleCard = ({ item, index, navigation }) => {
   return (
     <View
-      style={[styles.cardContainer, { paddingLeft: 20, paddingRight: 15}]} testID="articleDisplay"
+      style={[styles.cardContainer, { paddingLeft: 20, paddingRight: 15 }]}
+      testID="articleDisplay"
     >
-          <FlatList
-            data={articles}
-            keyExtractor={(item) => item.idArticle}
-            renderItem={renderItem}
-            numColumns={2}
-          />  
-
-        <TouchableOpacity
-            onPress={() => navigation.navigate("ArticleDetail", { ...item })}
-          >
-            <Image
-              source={{ uri: item.thumbnail }}
-              style={[styles.articleImage, { height: index % 3 === 0 ? hp(25) : hp(35) }]}
-            />
-            <Text style={styles.articleText}>
-              {item.title.length > 20 ? item.title.slice(0, 20) + "..." : item.title}
-            </Text>
-            <Text style={styles.articleDescription}>
-              {item.description.length > 40 ? item.description.slice(0, 40) + "..." : item.description}
-            </Text>
-        </TouchableOpacity>
-        
+      <TouchableOpacity
+        onPress={() => navigation.navigate("ArticleDetail", { ...item })}
+      >
+        <Image
+          source={{ uri: item.thumbnail }}
+          style={[
+            styles.articleImage,
+            { height: index % 3 === 0 ? hp(25) : hp(35) },
+          ]}
+        />
+        <Text style={styles.articleText}>
+          {item.title.length > 20
+            ? item.title.slice(0, 20) + "..."
+            : item.title}
+        </Text>
+        <Text style={styles.articleDescription}>
+          {item.description.length > 40
+            ? item.description.slice(0, 40) + "..."
+            : item.description}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: wp(4), // mx-4 equivalent
+    marginHorizontal: wp(4),
     marginTop: hp(2),
   },
   title: {
     fontSize: hp(3),
-    fontWeight: "600", // font-semibold
-    color: "#52525B", // text-neutral-600
+    fontWeight: "600",
+    color: "#52525B",
     marginBottom: hp(1.5),
   },
   loading: {
@@ -68,27 +88,27 @@ const styles = StyleSheet.create({
   cardContainer: {
     justifyContent: "center",
     marginBottom: hp(1.5),
-    flex: 1, // Allows cards to grow and fill space evenly
+    flex: 1,
   },
   articleImage: {
     width: "100%",
     borderRadius: 35,
-    backgroundColor: "rgba(0, 0, 0, 0.05)", // bg-black/5
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
   },
   articleText: {
     fontSize: hp(1.5),
-    fontWeight: "600", // font-semibold
-    color: "#52525B", // text-neutral-600
+    fontWeight: "600",
+    color: "#52525B",
     marginLeft: wp(2),
     marginTop: hp(0.5),
   },
   articleDescription: {
     fontSize: hp(1.2),
-    color: "#6B7280", // gray-500
+    color: "#6B7280",
     marginLeft: wp(2),
     marginTop: hp(0.5),
   },
   row: {
-    justifyContent: "space-between", // Align columns evenly
+    justifyContent: "space-between",
   },
 });
